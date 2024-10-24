fd8b381e2b0b16422faf0bcb7777b9f87e5e1c11
Franshel Tranetha <118864247+zanderxrf15@users.noreply.github.com>
2024-10-25 00:46:53 +0700

    Update wiki.go

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -15,12 +15,12 @@ type Page struct {
 }
 
 func (p *Page) save() error {
-	filename := p.Title + ".txt"
+	filename := "data/" + p.Title + ".txt"
 	return os.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
-	filename := title + ".txt"
+	filename := "data/" + title + ".txt"
 	body, err := os.ReadFile(filename)
 	if err != nil {
 		return nil, err
@@ -28,9 +28,19 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("tmpl/edit.html", "tmpl/view.html"))
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/view/FrontPage", http.StatusFound)
+}
 
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	re := regexp.MustCompile(`\[([a-zA-Z0-9]+)\]`)
+	p.Body = re.ReplaceAllFunc(p.Body, func(link []byte) []byte {
+		pageName := link[1 : len(link)-1]
+		return []byte(`<a href="/view/` + string(pageName) + `">` + string(pageName) + `</a>`)
+	})
+
 	err := templates.ExecuteTemplate(w, tmpl+".html", p)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -88,6 +98,9 @@ func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
 }
 
 func main() {
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("css"))))
+
+	http.HandleFunc("/", rootHandler)
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
